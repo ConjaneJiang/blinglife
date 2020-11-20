18302965b77b40a7b05043a7a62e627d941b8cf0
jiangkangjian <anonymous8090@sina.com>
2020-11-20 16:58:55 +0800

    博客列表页样式修改

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,11 +1,7 @@
 <template>
-  <el-container>
-    <el-container>
-      <el-aside width="200px">Left</el-aside>
-      <router-view name="blog"></router-view>
-      <el-aside width="200px">right</el-aside>
-    </el-container>
-  </el-container>
+  <div class="blog">
+    <router-view name="blog"></router-view>
+  </div>
 </template>
 
 <script>
@@ -15,37 +11,4 @@ export default {
 </script>
 
 <style scoped>
-.el-header, .el-footer {
-  background-color: #B3C0D1;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-}
-
-.el-aside {
-  background-color: #D3DCE6;
-  color: #333;
-  text-align: center;
-  line-height: 200px;
-}
-
-.el-main {
-  background-color: #E9EEF3;
-  color: #333;
-  text-align: center;
-  line-height: 160px;
-}
-
-body > .el-container {
-  margin-bottom: 40px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-  line-height: 320px;
-}
 </style>
--- src/views/BlogList.vue
+++ src/views/BlogList.vue
@@ -1,15 +1,65 @@
 <template>
   <div class="blogList">
-    <p><router-link :to="{path:'/blog/blogDetail/23'}">博客列表</router-link></p>
+    <div class="mainBoard">
+      <el-row>
+        <el-col :span="12" :offset="6">
+          <h2 @click="getBlogDetailById" class="title">Android 操作系统架构开篇Android 操作系统架构开篇Android </h2>
+        </el-col>
+        <el-col :span="12" :offset="6">
+          <div class="post-content-preview">2018 年 12 月 ，Google 宣布 Flutter 1.0 版本正式发布。截至目前， Flutter 在 Github 上已获得 88000+
+            的关注和 11000+ 的 Fork ，其发展速度相当惊人，是今年移动端最火热的开发框架之一。 Flutter 大火背后的原因是什么？为什么越来越多的企业和开发者会选择使用 Flutter？Flutter
+            会成为跨平台开发的终极...
+          </div>
+        </el-col>
+        <el-col :span="12" :offset="6">
+          <p>
+            Posted by Gityuan on January 31, 2016
+          </p>
+        </el-col>
+        <el-col :span="12" :offset="6">
+          <el-divider></el-divider>
+        </el-col>
+      </el-row>
+      <!--  分页组件    -->
+      <div style="text-align: center">
+        <el-pagination
+            layout="prev, pager, next"
+            :total=total>
+        </el-pagination>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
-name: "BlogList"
+  name: "BlogList",
+  data() {
+    return {
+      total:0
+    }
+  },
+  methods:{
+    getBlogDetailById(){
+      this.$router.push("/blog/blogDetail/3")
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.mainBoard {
+  width: 70%;
+  margin: 0 auto;
+}
+.title:hover {
+  color:#409EFF;
+  cursor: pointer;
+}
+.post-content-preview {
+  font-size: 13px;
+  font-style: italic;
+  color: #a3a3a3;
+}
 </style>
